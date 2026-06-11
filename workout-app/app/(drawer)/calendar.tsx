caac888b78cebecf782e413f0a0053094e0b3721
masongga <auckmason@gmail.com>
2026-06-10 18:39:18 -0600

    setting up initial mobile app

---
+++ workout-app/app/(drawer)/calendar.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Calendar() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "white" }}>Calendar</Text>
+    </View>
+  );
+}
---
+++ workout-app/app/(drawer)/skill-level.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SkillLevel() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "white" }}>Skill Level</Text>
+    </View>
+  );
+}
